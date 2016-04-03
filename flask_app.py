a3224e71b02ce0c0868028d62b23d918535e6a12
curioswati <jaiswalswati94@gmail.com>
2016-04-03 16:32:18 +0530

    creates the app with very basic functionality

---
+++ flask_app.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+from flask import Flask
+from flask import render_template
+from flask import redirect, url_for
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('message_form.html')
+
+
+@app.route('/send', methods=['POST', 'GET'])
+def send_message():
+    if request.method == 'POST':
+        message = request.form['message']
+
+        headers = {
+            'Content-type': "application/json"
+        }
+
+        data = json.dumps({
+            "text": message
+        })
+
+        # specific for slackbot
+        webhook_url = "https://hooks.slack.com/services/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"
+
+        req = requests.post(webhook_url, headers=headers, data=data)
+        print req
+
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
